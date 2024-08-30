caccd526bcca3ff84600a77914630ff9b0332f80
Raj <75741866+rajdama@users.noreply.github.com>
2024-08-30 16:42:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from pptx import Presentation
+from io import BytesIO
+from flask_cors import CORS 
+
+app = Flask(__name__)
+CORS(app) 
+
+@app.route('/', methods=['POST'])
+def update_ppt():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file or data provided'}), 400
+    file = request.files['file']
+
+    prs = Presentation(BytesIO(file.read()))
+
+    slides = {}
+    for slide_index, slide in enumerate(prs.slides):
+        slides[f'slide{slide_index+1}'] = {}
+        for shape in slide.shapes:
+            if shape.has_text_frame:
+                text_frame = shape.text_frame
+                for paragraph in text_frame.paragraphs:
+                    for run in paragraph.runs:
+                        txt = run.text.strip()
+                        slides[f'slide{slide_index+1}'][txt] = ""
+
+    modified_ppt = BytesIO()
+    prs.save(modified_ppt)
+    modified_ppt.seek(0)
+
+    return jsonify(slides)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
