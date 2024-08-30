a83cf787c9d35b35669f633cc042eb0617aaa4a3
Raj <75741866+rajdama@users.noreply.github.com>
2024-08-30 17:48:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file, jsonify
 from pptx import Presentation
 from io import BytesIO
 from flask_cors import CORS 
+from pptx.enum.lang import MSO_LANGUAGE_ID
 
 app = Flask(__name__)
 CORS(app) 
@@ -10,20 +11,24 @@ CORS(app)
 def update_ppt():
     if 'file' not in request.files:
         return jsonify({'error': 'No file or data provided'}), 400
+
     file = request.files['file']
 
-    prs = Presentation(BytesIO(file.read()))
 
+
+    prs = Presentation(BytesIO(file.read()))
+    
     slides = {}
     for slide_index, slide in enumerate(prs.slides):
-        slides[f'slide{slide_index+1}'] = {}
+        slides[f'slide{slide_index+1}'] = []
         for shape in slide.shapes:
             if shape.has_text_frame:
                 text_frame = shape.text_frame
                 for paragraph in text_frame.paragraphs:
                     for run in paragraph.runs:
+                        run.font.language_id = MSO_LANGUAGE_ID.POLISH
                         txt = run.text.strip()
-                        slides[f'slide{slide_index+1}'][txt] = ""
+                        slides[f'slide{slide_index+1}'].append(txt)
 
     modified_ppt = BytesIO()
     prs.save(modified_ppt)
